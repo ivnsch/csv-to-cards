8dcbb2a7af146347896ae10447806933fad1c7b4
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-23 06:51:07 +0100

    implement select column screen

--- src/SelectCols.tsx
+++ src/SelectCols.tsx
@@ -5,7 +5,91 @@ function SelectCols() {
   const filters = useStore((state) => state.filters);
   const toggleFilter = useStore((state) => state.toggleFilter);
 
-  return <div>todo</div>;
+  return (
+    <div>
+      <div>Select columns to show</div>
+      {Object.keys(filters).map((header) => (
+        <FilterRow
+          key={header}
+          header={header}
+          filters={filters}
+          toggleFilter={toggleFilter}
+        />
+      ))}
+    </div>
+  );
 }
 
+const FilterRow = ({
+  header,
+  filters,
+  toggleFilter,
+}: {
+  header: string;
+  filters: Record<string, boolean>;
+  toggleFilter: (header: string) => void;
+}) => {
+  return (
+    <div key={header} style={styles.filter}>
+      <button onClick={() => toggleFilter(header)} style={styles.checkbox}>
+        {filters[header] && <span style={styles.checkmark}>✔</span>}
+      </button>
+      <span style={styles.filterText}>{header}</span>
+    </div>
+  );
+};
+
 export default SelectCols;
+
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100,
+  },
+  header: {
+    color: "white",
+    marginBottom: 50,
+    fontSize: 20,
+  },
+  filter: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "100%",
+    marginBottom: 20,
+  },
+  containerInScrollView: {
+    flex: 1,
+    flexDirection: "column",
+    paddingBottom: 5,
+    alignItems: "flex-start",
+    width: "100%",
+  },
+  filterText: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    marginStart: 10,
+    fontSize: 20,
+  },
+  row: {
+    flexDirection: "row",
+    paddingVertical: 5,
+    borderBottomWidth: 1,
+    borderColor: "#333",
+  },
+  checkbox: {
+    width: 30,
+    height: 30,
+    borderWidth: 1,
+    borderColor: "#EAEAEA",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  checkmark: { color: "#BB86FC", fontSize: 20 },
+  startButton: {
+    bottom: 50,
+  },
+};
