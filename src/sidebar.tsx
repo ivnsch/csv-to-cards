fe09973237b3ef6c9e3cd883f1eae7388ee4bf9e
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-24 17:42:16 +0100

    bold selected navigation entry

--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -1,27 +1,46 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const SideBar = ({ isOpen }: { isOpen: boolean }) => {
-  const navigate = useNavigate();
-
   return (
     <div style={{ ...styles.sideBar, left: isOpen ? "0" : "-220px" }}>
-      <SideEntry text="Load CSV" onClick={() => navigate("/")} />
-      <SideEntry text="Settings" onClick={() => navigate("/card-settings")} />
-      <SideEntry text="Columns" onClick={() => navigate("/select-cols")} />
-      <SideEntry text="Cards" onClick={() => navigate("/pager")} />
+      <SideEntry text="Load CSV" path="/" />
+      <SideEntry text="Settings" path="/card-settings" />
+      <SideEntry text="Columns" path="/select-cols" />
+      <SideEntry text="Cards" path="/pager" />
     </div>
   );
 };
 
-const SideEntry = ({
+const SideEntry = ({ text, path }: { text: string; path: string }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <SideEntryInternal
+      text={text}
+      highlighted={location.pathname === path}
+      onClick={() => navigate(path)}
+    />
+  );
+};
+
+const SideEntryInternal = ({
   text,
+  highlighted,
   onClick,
 }: {
   text: string;
+  highlighted: boolean;
   onClick: () => void;
 }) => {
   return (
-    <div style={styles.entry} onClick={onClick}>
+    <div
+      style={{
+        ...styles.entry,
+        fontWeight: highlighted ? "bold" : "normal",
+      }}
+      onClick={onClick}
+    >
       {text}
     </div>
   );
